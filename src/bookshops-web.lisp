cf9f02fd2be29f434c72f7120828fe9cd7b633e3
vindarel <ehvince@mailz.org>
2018-02-17 02:09:28 +0100

    second test: list titles and prices from a web search: OK :)

--- src/bookshops-web.lisp
+++ src/bookshops-web.lisp
@@ -1,5 +1,6 @@
 (defpackage bookshops-web
   (:use #:cl
+        #:bookshops                     ;; local project
         #:weblocks/html)
     (:import-from #:weblocks/app
                 #:defapp))
@@ -12,16 +13,14 @@
 (defvar *port* (find-port:find-port))
 
 (defmethod weblocks/session:init ((app bookshops))
-  (let ((books '("Antigone"
-                 "Feu"
-                 "Krop")))
+  (let ((books (books "antigone bauchau")))
     (lambda ()
       (with-html
         (:h1 "Books")
         (:ul :class "books"
              (loop for it in books
                 do (with-html
-                     (:li it))))))))
+                     (:li (format nil "~a ~a" (title it) (price it))))))))))
 
 (defun start ()
   (weblocks/server:start :port *port*))
