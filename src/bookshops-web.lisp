df8eee5f9821b99f9934c10661b083e1c5e26708
vindarel <ehvince@mailz.org>
2018-02-17 02:07:43 +0100

    first test: with a list of strings: ok

--- src/bookshops-web.lisp
+++ src/bookshops-web.lisp
@@ -5,3 +5,23 @@
                 #:defapp))
 (in-package :bookshops-web)
 
+(defapp bookshops)
+
+(weblocks/debug:on)
+
+(defvar *port* (find-port:find-port))
+
+(defmethod weblocks/session:init ((app bookshops))
+  (let ((books '("Antigone"
+                 "Feu"
+                 "Krop")))
+    (lambda ()
+      (with-html
+        (:h1 "Books")
+        (:ul :class "books"
+             (loop for it in books
+                do (with-html
+                     (:li it))))))))
+
+(defun start ()
+  (weblocks/server:start :port *port*))
