6864576eddcbb4705a0e9c92f842899baf2186c0
vindarel <ehvince@mailz.org>
2018-02-21 22:42:35 +0100

    search and display any book ! \o/

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Bookshops-Web
 
+https://github.com/40ants/weblocks/blob/reblocks/docs/source/quickstart.rst
+
+https://gitlab.com/vindarel/cl-bookshops
+
 ## Usage
 
+    (weblocks/debug:reset-latest-session)
+
+    (start)
+
+
 ## Installation
+
+Needs weblocks (reblocks), weblocks-ui, weblocks-parenscript & our cl-bookshops in QL's local projects.
--- src/bookshops-web.lisp
+++ src/bookshops-web.lisp
@@ -1,26 +1,40 @@
 (defpackage bookshops-web
   (:use #:cl
-        #:bookshops                     ;; local project
+        #:bookshops ;; local project
         #:weblocks/html)
-    (:import-from #:weblocks/app
-                #:defapp))
+  (:import-from #:weblocks/app
+                #:defapp)
+  (:import-from #:weblocks-ui/form
+                #:with-html-form
+                #:render-form-and-button))
 (in-package :bookshops-web)
 
 (defapp bookshops)
 
-(weblocks/debug:on)
-
 (defvar *port* (find-port:find-port))
 
+(defparameter *results* nil
+  "List of books.")
+
+
 (defmethod weblocks/session:init ((app bookshops))
-  (let ((books (books "antigone bauchau")))
+  (flet ((query (&key query &allow-other-keys)
+           (setf *results* (books query))
+           (weblocks/widget:update (weblocks/widgets/root:get))))
     (lambda ()
       (with-html
         (:h1 "Books")
+        (with-html-form (:POST #'query)
+          (:input :type "text"
+                  :name "query"
+                  :placeholder "search...")
+          (:input :type "submit"
+                  :value "Search"))
         (:ul :class "books"
-             (loop for it in books
+             (loop for it in *results*
                 do (with-html
                      (:li (format nil "~a ~a" (title it) (price it))))))))))
 
 (defun start ()
+  (weblocks/debug:on)
   (weblocks/server:start :port *port*))
