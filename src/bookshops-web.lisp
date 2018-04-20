aeab9ce46fbd24e9710166084a50ed3dbd3de04d
vindarel <ehvince@mailz.org>
2018-04-20 13:43:57 +0200

    button to toggle covers (full refresh, needs widget update)

--- src/bookshops-web.lisp
+++ src/bookshops-web.lisp
@@ -28,13 +28,18 @@
 
 
 (defmethod weblocks/session:init ((app bookshops))
-  (let (results)
+  (let (results
+        (show-covers t))
     (flet ((query (&key query &allow-other-keys)
              (setf results (books query))
              (weblocks/widget:update (weblocks/widgets/root:get)))
            (add-book (&key index &allow-other-keys)
              ;; logging
              (save-book (elt results (parse-integer index)))
+             (weblocks/widget:update (weblocks/widgets/root:get)))
+
+           (toggle-covers ()
+             (setf show-covers (not show-covers))
              (weblocks/widget:update (weblocks/widgets/root:get))))
       (lambda ()
         (with-html
@@ -50,41 +55,49 @@
            (:h1 "Minibookshop")
            (with-html-form (:POST #'query)
              (:div :class "ui action input"
-              (:input :type "text"
-                      :name "query"
-                      :class "ui input"
-                      :placeholder "search...")
-              (:input :type "submit"
-                      :value "Search")))
+                   (:input :type "text"
+                           :name "query"
+                           :class "ui input"
+                           :placeholder "search...")
+                   (:input :type "submit"
+                           :value "Search")))
+
+           (:button :class "ui basic button"
+                    :onclick (weblocks/actions:make-js-action
+                              (lambda (&rest rest)
+                                (declare (ignore rest))
+                                (toggle-covers)))
+            (:i :class "list icon"))
 
            (:table :class "ui selectable table"
-            (:thead
-             (:th "Title")
-             (:th "Authors")
-             (:th "Editor")
-             (:th "Price")
-             (:th "Quantity")
-             (:th "Add-Book ?")
-             (:th ))
-            (:tbody)
-            (dolist (it results)
-              (with-html
-                (:tr
-                 (:td
-                  (:div (title it))
-                  (:img :src (cover-url it)))
-                 (:td (authors it))
-                 (:td (editor it))
-                 (:td (price it))
-                 (:td (quantity-of it))
-                 (:td
-                  (with-html-form (:POST #'add-book)
-                    (:input :type "hidden"
-                            :name "index"
-                            :value (position it results))
-                    (:input :type "submit"
-                            :class "ui primary button"
-                            :value "add-book")))))))))))))
+                   (:thead
+                    (:th "Title")
+                    (:th "Authors")
+                    (:th "Editor")
+                    (:th "Price")
+                    (:th "Quantity")
+                    (:th "Add-Book ?")
+                    (:th ))
+                   (:tbody)
+                   (dolist (it results)
+                     (with-html
+                       (:tr
+                        (:td
+                         (:div (title it))
+                         (when show-covers
+                           (:img :src (cover-url it))))
+                        (:td (authors it))
+                        (:td (editor it))
+                        (:td (price it))
+                        (:td (quantity-of it))
+                        (:td
+                         (with-html-form (:POST #'add-book)
+                           (:input :type "hidden"
+                                   :name "index"
+                                   :value (position it results))
+                           (:input :type "submit"
+                                   :class "ui primary button"
+                                   :value "add-book")))))))))))))
 
 (defun start ()
   (weblocks/debug:on)
